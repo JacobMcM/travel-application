8f28e3d3d60e2d8eaebe13b7d8c4b35aa8f660a4
Jacob McMullen <jmcmcullen@bayviewglen.ca>
2022-04-29 12:36:48 -0400

    Travel App

---
+++ src/components/TheNavigationBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="`/destination/${destination.id}`"
+    >
+      {{ destination.name }}
+    </router-link>
+  </div>
+</template>
+ 
+<script>
+import sourceData from "@/data.json";
+export default {
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   {
@@ -10,11 +10,13 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: ()=> import('@/views/AboutView.vue')
+  },
+  {
+    path: '/destination/:id',
+    name: 'destination.show',
+    component: ()=> import('@/views/DestinationView.vue')
+  },
 ]
 
 const router = createRouter({
